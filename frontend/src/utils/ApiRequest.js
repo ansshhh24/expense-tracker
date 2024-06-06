10af145528fea7ee859ea56eee8fc3e49fcf4e93
Ansh Sharma <86212182+ansshhh24@users.noreply.github.com>
2024-06-06 09:33:32 +0530

    Update ApiRequest.js

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,4 +1,4 @@
-const host = "http://localhost:5000";
+const host = "https://expense-tracker-ekwv.onrender.com";
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
 export const loginAPI = `${host}/api/auth/login`;
